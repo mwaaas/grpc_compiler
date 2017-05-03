1b744a7057f09de26ebec932d458fdc5f091f335
mwas <francismwangi152@gmail.com>
2017-05-03 22:27:52 +0300

    adding command to install requirements

--- build_lambda.sh
+++ build_lambda.sh
@@ -7,7 +7,7 @@ if [ $( ls /dist/ ) ]; then rm /dist/*.zip; fi
 
 cp -R ./ /tmp/build/  # add the source code to a build environment
 
-#pip3.6 install -r ./requirements.txt -t /tmp/build/
+pip3.6 install -r ./requirements.txt -t /tmp/build/
 
 cd /tmp/build/ && zip /usr/src/app/dist/bundle.zip -r .
 
